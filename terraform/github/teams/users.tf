fe68f3859a29268efcdec2929f1eabda4c36a6f7
Scott Melhop <scott.melhop@cognite.com>
2021-11-07 13:56:55 +0100

    Fix github locals

--- terraform/github/teams/users.tf
+++ terraform/github/teams/users.tf
@@ -1,13 +1,15 @@
-cognite_noc_user_map = {
+locals {
+  user_map = {
 
-  "scott.melhop@cognitedata.com" = {
-    "github_account" = "scottmelhop"
-    "org_member"     = "admin"
+    "scott.melhop@cognitedata.com" = {
+      "github_account" = "scottmelhop"
+      "org_member"     = "admin"
+    }
+
+    "github@skfcenitbycognite.onmicrosoft.com" = {
+      "github_account" = "cognite-skfcenit-cicd"
+      "org_member"     = "admin"
+    }
+
   }
-
-  "github@skfcenitbycognite.onmicrosoft.com" = {
-    "github_account" = "cognite-skfcenit-cicd"
-    "org_member"     = "admin"
-  }
-
 }
