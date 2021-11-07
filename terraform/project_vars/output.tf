452fc8478a4ea736e79f4a8f44659b65c761162f
Scott Melhop <scott.melhop@cognite.com>
2021-11-07 12:40:22 +0100

    Try again

--- terraform/project_vars/output.tf
+++ terraform/project_vars/output.tf
@@ -1,4 +1,4 @@
-# When using Azure Storage Account, set the following
+# When using Azure Storage Account, set the following output
 output "storage_account_name" {
   value = "cogniteskfcenitterraform"
 }
