f6973e85014bdeadb51ecdba988901da4f76297a
Scott Melhop <scott.melhop@cognite.com>
2021-11-08 08:01:15 +0100

    Try test team

---
+++ terraform/github/teams/test_team.tf
@@ -0,0 +1,15 @@
+# Create a team
+resource "github_team" "team_test" {
+  name        = "test-team"
+  description = "Test Team creation"
+  privacy     = "closed"
+}
+
+# Add a user member to a team
+# resource "github_team_membership" "team_noc_employee_write_membership" {
+#   for_each = { for k, v in local.cognite_noc_user_map : k => v if contains(keys(v), "team_noc_employee_write") }
+
+#   team_id  = github_team.team_noc_employee_write.id
+#   username = local.cognite_noc_user_map[each.key].github_account
+#   role     = local.cognite_noc_user_map[each.key].team_noc_employee_write
+# }
--- terraform/github/teams/users.tf
+++ terraform/github/teams/users.tf
@@ -1,15 +1,15 @@
 locals {
   user_map = {
 
-    "scott.melhop@cognitedata.com" = {
-      "github_account" = "scottmelhop"
-      "org_member"     = "admin"
-    }
+    # "scott.melhop@cognitedata.com" = {
+    #   "github_account" = "scottmelhop"
+    #   "org_member"     = "admin"
+    # }
 
-    "github@skfcenitbycognite.onmicrosoft.com" = {
-      "github_account" = "cognite-skfcenit-cicd"
-      "org_member"     = "admin"
-    }
+    # "github@skfcenitbycognite.onmicrosoft.com" = {
+    #   "github_account" = "cognite-skfcenit-cicd"
+    #   "org_member"     = "admin"
+    # }
 
   }
 }
