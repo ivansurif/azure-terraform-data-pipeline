d84a2f1831306437600b6926c3b75bb37deb8078
Scott Melhop <scott.melhop@cognite.com>
2021-11-08 14:58:36 +0100

    more naming

--- terraform/github/teams/org_member.tf
+++ terraform/github/teams/org_member.tf
@@ -1,5 +1,5 @@
 # Add a user to the organization
-resource "github_membership" "membership_for_noc" {
+resource "github_membership" "org_membership" {
   for_each = module.project_vars.github_users
 
   username = each.value.github_account
