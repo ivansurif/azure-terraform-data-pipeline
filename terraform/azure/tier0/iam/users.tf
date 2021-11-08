0bbe10551cea8f46bf7c5e40827acb2451b8279f
Scott Melhop <scott.melhop@cognite.com>
2021-11-08 14:36:28 +0100

    Fix problem with data

--- terraform/azure/tier0/iam/users.tf
+++ terraform/azure/tier0/iam/users.tf
@@ -2,10 +2,6 @@ data "azuread_users" "users" {
   return_all = true
 }
 
-# locals {
-#   users = {for u in data.azuread_users.users.users : u.mail => u.object_id}
-# }
-
-# output "name" {
-#   value = local.users
-# }
+locals {
+  users = {for u in data.azuread_users.users.users : u.mail => u.object_id}
+}
