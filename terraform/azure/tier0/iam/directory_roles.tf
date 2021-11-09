6d70f354e5b07a22ae0e769419e2ef6865b83441
Sirefelt <joel.c.sirefelt@accenture.com>
2021-11-09 15:48:18 +0100

    Add user admin role

--- terraform/azure/tier0/iam/directory_roles.tf
+++ terraform/azure/tier0/iam/directory_roles.tf
@@ -6,10 +6,24 @@ locals {
   app_admins = toset([
       "joel.sirefelt@cognitedata.com"
   ])
+
+  user_admins = toset([
+      "joel.sirefelt@cognitedata.com"
+  ])
 }
 
 resource "azuread_directory_role_member" "app_admin_members" {
   for_each = local.app_admins
   role_object_id   = azuread_directory_role.app_admin.object_id
+  member_object_id = local.users[each.value]
+}
+
+resource "azuread_directory_role" "user_admin" {
+  display_name = "User Access Administrator"
+}
+
+resource "azuread_directory_role_member" "user_admin_members" {
+  for_each = local.user_admins
+  role_object_id   = azuread_directory_role.user_admin.object_id
   member_object_id = local.users[each.value]
 }
