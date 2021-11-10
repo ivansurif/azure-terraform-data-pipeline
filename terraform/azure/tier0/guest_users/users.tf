319d766bf955a343b654b849c9649e4673e17321
Scott Melhop <scott.melhop@cognite.com>
2021-11-10 08:15:21 +0100

    Attempt to add HVT

---
+++ terraform/azure/tier0/guest_users/users.tf
@@ -0,0 +1,16 @@
+locals {
+  guest_users = {
+      "Håkon Treider" : "hakon.treider@cognitedata.com"
+  }
+}
+
+resource "azuread_invitation" "example" {
+  for_each = local.guest_users
+  user_display_name  = each.key
+  user_email_address = each.value
+  redirect_url       = "https://portal.azure.com"
+
+  message {
+    body                  = "Hi, welcome to Cognite - SKF Cenit Azure AD Tenant"
+  }
+}
