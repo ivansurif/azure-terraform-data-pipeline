26a317d557a624e35ec2f7684489223d7b54c81e
Scott Melhop <scott.melhop@cognite.com>
2021-11-10 11:31:58 +0100

    Add Hakon as a member

--- terraform/azure/tier0/guest_users/users.tf
+++ terraform/azure/tier0/guest_users/users.tf
@@ -1,16 +1,16 @@
 locals {
   guest_users = {
-      "Håkon Treider" : "hakon.treider@cognitedata.com"
+    "Håkon Treider" : "hakon.treider@cognitedata.com"
   }
 }
 
 resource "azuread_invitation" "example" {
-  for_each = local.guest_users
+  for_each           = local.guest_users
   user_display_name  = each.key
   user_email_address = each.value
   redirect_url       = "https://portal.azure.com"
 
   message {
-    body                  = "Hi, welcome to Cognite - SKF Cenit Azure AD Tenant"
+    body = "Hi, welcome to Cognite - SKF Cenit Azure AD Tenant"
   }
 }
---
+++ terraform/github/teams/users.tf
@@ -0,0 +1,37 @@
+locals {
+  github_users = {
+
+    "github@skfcenitbycognite.onmicrosoft.com" = {
+      "github_account" = "cognite-skfcenit-cicd"
+      "org_member"     = "admin"
+    }
+
+    "hakon.treider@cognitedata.com" = {
+      "github_account" = "haakonvt"
+      "org_member"     = "member"
+    }
+
+    "joel.sirefelt@cognitedata.com" = {
+      "github_account"  = "CogJoel"
+      "org_member"      = "member"
+      "infra_team_push" = "member"
+    }
+
+    "scott.melhop@cognitedata.com" = {
+      "github_account" = "scottmelhop"
+      "org_member"     = "admin"
+    }
+
+    "ivan.surif@cognitedata.com" = {
+      "github_account"  = "ivansurif"
+      "org_member"      = "member"
+      "infra_team_push" = "member"
+    }
+
+    "igor.suchilov@cognitedata.com" = {
+      "github_account"  = "igors-cognite"
+      "org_member"      = "member"
+      "infra_team_push" = "member"
+    }
+  }
+}
