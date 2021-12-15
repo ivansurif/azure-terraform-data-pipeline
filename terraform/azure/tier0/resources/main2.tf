32b10dcf54f1a82a70018ea6f56eee0b5ecc885e
Iván <ivan@Ivans-MacBook-Pro.local>
2021-12-15 07:23:39 -0300

    Corrected formatting issue

--- terraform/azure/tier0/resources/main2.tf
+++ terraform/azure/tier0/resources/main2.tf
@@ -20,7 +20,7 @@ resource "azurerm_key_vault" "kv" {
 }
 
 
-  resource "azurerm_key_vault_access_policy" "kv_ap" {
+resource "azurerm_key_vault_access_policy" "kv_ap" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
@@ -28,17 +28,17 @@ resource "azurerm_key_vault" "kv" {
   key_permissions = [
     "Get",
     "Set"
-  ],
+  ]
 
   secret_permissions = [
     "Get",
     "Set"
-  ],
+  ]
 
   storage_permissions = [
-      "Get",
-      "Set"
-    ]
+    "Get",
+    "Set"
+  ]
 }
 
 
