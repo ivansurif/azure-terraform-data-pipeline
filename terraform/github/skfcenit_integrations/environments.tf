a2137e0a16eaa5a2d9833d886802a81fe39c1146
Scott Melhop <scott.melhop@cognite.com>
2021-12-21 16:26:50 +0100

    Create json rbac creds in each environment

--- terraform/github/skfcenit_integrations/environments.tf
+++ terraform/github/skfcenit_integrations/environments.tf
@@ -3,14 +3,21 @@ locals {
     dev = {
       teams = []
       users = []
+      protected_branches = false
+      resource_group_name = "integration-functions-dev"
     }
     test = {
       teams = [data.github_team.infra_team.id]
       users = [data.github_user.scott.id]
+      protected_branches = true
+      resource_group_name = "integration-functions-test"
+
     }
     prod = {
       teams = [data.github_team.infra_team.id]
       users = [data.github_user.scott.id]
+      protected_branches = true
+      resource_group_name = "integration-functions-prod"
     }
   }
 }
@@ -24,12 +31,23 @@ resource "github_repository_environment" "environments" {
     teams = each.value["teams"]
     users = each.value["users"]
   }
+  deployment_branch_policy {
+    protected_branches     = each.value["protected_branches"]
+    custom_branch_policies = false
+  }
 }
 
 resource "github_actions_environment_secret" "site_creds" {
   for_each        = local.environments
   repository      = data.github_repository.repo.name
   environment     = github_repository_environment.environments[each.key].environment
-  secret_name     = "SITE_CREDENTIALS"
-  plaintext_value = jsonencode(data.terraform_remote_state.integrations.outputs.site_credentials)
+  secret_name     = "AZURE_RBAC_CREDENTIALS"
+  plaintext_value = jsonencode(
+    {
+      "clientId": data.terraform_remote_state.integrations.outputs.sp_credentials[each.value["resource_group_name"]["CLIENT_ID"]],
+      "clientSecret": data.terraform_remote_state.integrations.outputs.sp_credentials[each.value["resource_group_name"]["CLIENT_SECRET"]],
+      "subscriptionId": data.terraform_remote_state.integrations.outputs.sp_credentials[each.value["resource_group_name"]["SUBSCIPTION_ID"]],
+      "tenantId": data.terraform_remote_state.integrations.outputs.sp_credentials[each.value["resource_group_name"]["TENANT_ID"]],
+    }
+  )
 }
