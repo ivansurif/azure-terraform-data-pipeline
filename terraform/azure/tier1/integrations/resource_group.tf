113d90c92c139527264b2a34f0dbb9297abf2a78
Scott Melhop <scott.melhop@cognite.com>
2021-12-21 15:54:55 +0100

    fix for

--- terraform/azure/tier1/integrations/resource_group.tf
+++ terraform/azure/tier1/integrations/resource_group.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  for_each = local.resource_group_names
+  foreach = local.resource_group_names
   name     = each.value
   location = local.resource_group_location
 }
