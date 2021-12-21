03c8c2ddc05c398f71d4b212be78080b108551e6
Scott Melhop <scott.melhop@cognite.com>
2021-12-21 15:18:04 +0100

    Create each environment and add site creds

---
+++ terraform/github/skfcenit_integrations/environments.tf
@@ -0,0 +1,35 @@
+locals {
+  environments = {
+    dev = {
+      teams = []
+      users = []
+    }
+    test = {
+      teams = [data.github_team.infra_team.id]
+      users = [data.github_user.scott.id]
+    }
+    prod = {
+      teams = [data.github_team.infra_team.id]
+      users = [data.github_user.scott.id]
+    }
+  }
+}
+
+
+resource "github_repository_environment" "environments" {
+  for_each = local.environments
+  environment = "${each.key}-integrations"
+  repository  = data.github_repository.repo.name
+  reviewers {
+    teams = each.value["teams"]
+    users = each.value["users"]
+  }
+}
+
+resource "github_actions_environment_secret" "site_creds" {
+  for_each = local.environments
+  repository      = data.github_repository.repo.name
+  environment     = github_repository_environment.test_environment[each.key].environment
+  secret_name     = "SITE_CREDENTIALS"
+  plaintext_value = data.terraform_remote_state.integrations.outputs.site_credentials
+}
