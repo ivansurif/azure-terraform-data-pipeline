e663962432021f89769c419a1b0055256dd07ca5
Sirefelt <joel.c.sirefelt@accenture.com>
2021-12-23 10:54:49 +0100

    Add aks app registration to be used for batch job deployments

--- terraform/azure/tier0/common_services/container_registry.tf
+++ terraform/azure/tier0/common_services/container_registry.tf
@@ -5,3 +5,10 @@ resource "azurerm_container_registry" "acr" {
   sku                 = local.container_registry_sku
   admin_enabled       = true
 }
+
+
+resource "azurerm_role_assignment" "role_aks_app_to_acr" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_container_registry.acr.id
+}
