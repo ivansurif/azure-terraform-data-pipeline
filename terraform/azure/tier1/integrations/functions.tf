d3bce73953cd36cc33ebe3f15518632cee9d8d41
Iván <ivan@Ivans-MacBook-Pro.local>
2021-12-28 13:51:39 -0300

    created function apps for file upload functions

--- terraform/azure/tier1/integrations/functions.tf
+++ terraform/azure/tier1/integrations/functions.tf
@@ -29,6 +29,37 @@ resource "azurerm_function_app" "functions" {
   }
 }
 
+resource "azurerm_function_app" "files_upload_functions" {
+  for_each                   = local.files_upload_function_apps
+  name                       = each.key
+  location                   = azurerm_resource_group.rg[each.value["resource_group_name"]].location
+  resource_group_name        = azurerm_resource_group.rg[each.value["resource_group_name"]].name
+  app_service_plan_id        = data.terraform_remote_state.common_services.outputs.app_service_plan_id
+  storage_account_name       = local.storage_account_name
+  storage_account_access_key = local.primary_blob_access_key
+  os_type                    = "linux"
+  version                    = "~3"
+
+
+  app_settings = merge(
+    {
+      APPINSIGHTS_INSTRUMENTATIONKEY = data.terraform_remote_state.common_services.outputs.insights_instrumentation_key
+    },
+    each.value["app_settings"],
+    { for key, secret in each.value["secrets"] : key => "@Microsoft.KeyVault(VaultName=${local.key_vault_name};SecretName=${secret})" }
+  )
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    always_on        = each.value["always_on"]
+    linux_fx_version = each.value["linux_fx_version"]
+    scm_type         = "LocalGit"
+  }
+}
+
 resource "azurerm_key_vault_access_policy" "function_app_policies" {
   for_each     = local.function_apps
   key_vault_id = local.key_vault_id
