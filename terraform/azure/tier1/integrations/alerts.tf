8d6a96ccb536bbbc55c64b2183a0577c1f84b4d5
Sirefelt <joel.c.sirefelt@accenture.com>
2022-01-07 14:06:55 +0100

    Add alert for all functions

---
+++ terraform/azure/tier1/integrations/alerts.tf
@@ -0,0 +1,24 @@
+resource "azurerm_monitor_scheduled_query_rules_alert" "function-alerts" {
+  for_each            = local.alerts
+  name                = each.key
+  location            = data.terraform_remote_state.common_services.outputs.resource_group_location
+  resource_group_name = data.terraform_remote_state.common_services.outputs.resource_group_name
+
+  action {
+    action_group = [
+      data.terraform_remote_state.common_services.outputs.action_group_id
+    ]
+  }
+  data_source_id = data.terraform_remote_state.common_services.outputs.insights_id
+  description    = "Exception threshold reached"
+  enabled        = true
+  # Count all requests with server error result code grouped into 5-minute bins
+  query       = each.value["query"]
+  severity    = 1
+  frequency   = 10
+  time_window = 10
+  trigger {
+    operator  = "LessThan"
+    threshold = 1
+  }
+}
