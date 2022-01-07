abd8aed0a39823109d75dfad92e9a5645e115859
Sirefelt <joel.c.sirefelt@accenture.com>
2022-01-07 12:00:14 +0100

    Add alert for poz dev function

--- terraform/azure/tier0/common_services/application_insights.tf
+++ terraform/azure/tier0/common_services/application_insights.tf
@@ -9,6 +9,7 @@ resource "azurerm_application_insights" "insights" {
 resource "azurerm_monitor_action_group" "action_group" {
   name                = "function-alerts"
   resource_group_name = azurerm_resource_group.common.name
+  short_name          = "f-alerts"
 
   email_receiver {
     name                    = "sendtojoel"
