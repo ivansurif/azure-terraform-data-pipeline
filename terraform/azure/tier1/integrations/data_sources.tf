e873b07e8cdfdda75b8fed52454bc82dfb04dc40
Sirefelt <joel.c.sirefelt@accenture.com>
2022-01-27 15:38:41 +0100

    Add skf ai write back functions

--- terraform/azure/tier1/integrations/data_sources.tf
+++ terraform/azure/tier1/integrations/data_sources.tf
@@ -63,6 +63,7 @@ locals {
 
   common_secrets = {
     API_KEY = "API-KEY"
+    TOPIC_ACCESS_KEY = "WRITE_BACK_KEY"
   }
 
   environments = {
@@ -163,4 +164,18 @@ locals {
       resource_group_name = local.environments[app["environment"]]["resource_group_name_files_upload"]
     }
   }
+
+  function_skf_ai_data = {
+    for app in local.apps_files_upload :
+    "skf_ai_data-${app["environment"]}" => {
+      app_service_plan    = data.terraform_remote_state.common_services.outputs.app_service_plan_name_files_upload
+      app_settings        = merge(local.environments[app["environment"]]["app_settings"], local.common_app_settings, {URL_TOPIC = "https://transportecenit.azurefd.net/ub/api/events"})
+      secrets             = merge(local.environments[app["environment"]]["secrets"], local.common_secrets)
+      always_on           = false
+      https_only          = true
+      linux_fx_version    = "Python|3.9"
+      resource_group_name = local.environments[app["environment"]]["resource_group_name_files_upload"]
+    }
+  }
 }
+
--- terraform/azure/tier1/integrations/functions.tf
+++ terraform/azure/tier1/integrations/functions.tf
@@ -1,5 +1,5 @@
 resource "azurerm_function_app" "functions" {
-  for_each                   = local.function_apps
+  for_each                   = local.apps_files_upload
   name                       = each.key
   location                   = azurerm_resource_group.rg[each.value["resource_group_name"]].location
   resource_group_name        = azurerm_resource_group.rg[each.value["resource_group_name"]].name
@@ -52,6 +52,38 @@ resource "azurerm_function_app" "functions_files_upload" {
   identity {
     type = "SystemAssigned"
   }
+  site_config {
+    always_on        = each.value["always_on"]
+    linux_fx_version = each.value["linux_fx_version"]
+    scm_type         = "LocalGit"
+  }
+}
+
+
+resource "azurerm_function_app" "skf-ai-data" {
+  for_each                   = local.skf_ai_data
+  name                       = each.key
+  location                   = azurerm_resource_group.rg[each.value["resource_group_name"]].location
+  resource_group_name        = azurerm_resource_group.rg[each.value["resource_group_name"]].name
+  app_service_plan_id        = data.terraform_remote_state.common_services.outputs.app_service_plan_id
+  storage_account_name       = local.storage_account_name
+  storage_account_access_key = local.primary_blob_access_key
+  os_type                    = "linux"
+  version                    = "~3"
+
+
+  app_settings = merge(
+    {
+      APPINSIGHTS_INSTRUMENTATIONKEY = data.terraform_remote_state.common_services.outputs.insights_instrumentation_key
+    },
+    each.value["app_settings"],
+    { for key, secret in each.value["secrets"] : key => "@Microsoft.KeyVault(VaultName=${local.key_vault_name};SecretName=${secret})" }
+  )
+
+  identity {
+    type = "SystemAssigned"
+  }
+
   site_config {
     always_on        = each.value["always_on"]
     linux_fx_version = each.value["linux_fx_version"]
