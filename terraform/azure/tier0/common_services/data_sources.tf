2adae99de70cde25c1d2a75e68b2f3a72a70d9ad
Iván <ivan@Ivans-MacBook-Pro.local>
2022-02-04 08:12:26 -0300

    remove placeholder variable

--- terraform/azure/tier0/common_services/data_sources.tf
+++ terraform/azure/tier0/common_services/data_sources.tf
@@ -8,7 +8,6 @@ data "azurerm_client_config" "current" {}
 locals {
 
   # Resource Group
-  placeholder_variable = "test"
   resource_group_name     = "common-services"
   resource_group_location = module.project_vars.location
   # Resource Group
