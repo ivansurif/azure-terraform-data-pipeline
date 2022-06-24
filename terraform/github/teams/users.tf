12e521f62660bce140188b77c9b682e1c76542f5
Ivan Surif <ivansurif@MacBook-Pro.local>
2022-06-23 23:33:53 -0300

    set up GH via Terraform

--- terraform/github/teams/users.tf
+++ terraform/github/teams/users.tf
@@ -6,58 +6,9 @@ locals {
       "org_member"     = "admin"
     }
 
-    "hakon.treider@cognitedata.com" = {
-      "github_account" = "haakonvt"
-      "org_member"     = "member"
-    }
-
-    "kelvin.sundli@cognitedata.com" = {
-      "github_account" = "ks93"
-      "org_member"     = "member"
-    }
-
-    "joel.sirefelt@cognitedata.com" = {
-      "github_account"  = "CogJoel"
-      "org_member"      = "member"
-      "infra_team_push" = "member"
-    }
-
-    "scott.melhop@cognitedata.com" = {
-      "github_account" = "scottmelhop"
-      "org_member"     = "admin"
-    }
-
     "ivan.surif@cognitedata.com" = {
       "github_account"  = "ivansurif"
       "org_member"      = "admin"
-    }
-    
-    "david.alvarez@cognitedata.com" = {
-      "github_account"  = "david-alvarez-cognite"
-      "org_member"      = "member"
-    }
-
-    "igor.suchilov@cognitedata.com" = {
-      "github_account"  = "igors-cognite"
-      "org_member"      = "member"
-      "infra_team_push" = "member"
-    }
-  }
-
-  external_collaborators = {
-    "cognite.test.user@gmail.com" = {
-      "github_account"  = "gognite-test-user"
-      "org_member"      = "member"
-    }
-
-    "agustin.canto@skf.com" = {
-      "github_account"  = "acanto29"
-      "org_member"      = "member"
-    }
-
-    "claudio.guerrero@skf.com" = {
-      "github_account"  = "c-dasq"
-      "org_member"      = "member"
     }
 
     "david.mendez@skf.com" = {
@@ -65,18 +16,11 @@ locals {
       "org_member"      = "member"
     }
 
-    "leandro.torrent@skf.com" = {
-      "github_account"  = "leandrotorrent"
-      "org_member"      = "member"
-    }
+  }
 
-    "osmel.cardenas@skf.com" = {
-      "github_account"  = "Rafael-Cardenas-P"
-      "org_member"      = "member"
-    }
-
-    "marco.santos@skf.com" = {
-      "github_account"  = "darkrori"
+  external_collaborators = {
+    "cognite.test.user@gmail.com" = {
+      "github_account"  = "gognite-test-user"
       "org_member"      = "member"
     }
   }
