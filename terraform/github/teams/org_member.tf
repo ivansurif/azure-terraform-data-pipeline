3847f555d2b67a4deb159610788d4cd9f521c819
Ivan Surif <ivansurif@MacBook-Pro.local>
2022-06-25 10:59:48 -0300

    remove GitHub from terraform

--- terraform/github/teams/org_member.tf
+++
@@ -1,7 +0,0 @@
-# Add a user to the organization
-resource "github_membership" "org_membership" {
-  for_each = local.github_users
-
-  username = each.value.github_account
-  role     = each.value.org_member
-}
