1331118f5ee6140270921eaa5bd94af829f31bd1
Ivan Surif <ivansurif@MacBook-Pro-4.local>
2023-02-11 21:00:01 -0300

    fix retrieving all users from AAD

--- terraform/azure/tier0/guest_users/read_users.tf
+++ terraform/azure/tier0/guest_users/read_users.tf
@@ -1,7 +1,7 @@
-data "azuread_user" "example_users" {
-  count = null
-}
+data "azuread_users" "example" {}
 
-output "user_display_names" {
-  value = [for user in data.azuread_user.example_users : user.display_name]
+output "user_names" {
+  value = {
+    for user in data.azuread_users.example.users : user.user_principal_name => user.display_name
+  }
 }
