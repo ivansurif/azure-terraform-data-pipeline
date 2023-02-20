4c65e033e8e405abbb2bea62e19a6128166635db
Ivan Surif <ivansurif@MacBook-Pro-4.local>
2023-02-19 21:12:05 -0300

    update hardcoded user principal name

--- terraform/azure/tier0/guest_users/read_users.tf
+++ terraform/azure/tier0/guest_users/read_users.tf
@@ -1,6 +1,6 @@
 resource "azuread_user" "example" {
   # Add existing user to Terraform State File
-  user_principal_name = "cognite.test.user@gmail.com"
+  user_principal_name = "cognite.test.user_gmail.com#EXT#@ivansurifgmail.onmicrosoft.com"
   display_name        = "Test User"
 
 }
