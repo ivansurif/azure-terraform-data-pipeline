bb729e4a6ff2e93f54285e0404c0de879e9e1c4f
Ivan Surif <ivansurif@MacBook-Pro-4.local>
2023-02-19 21:50:21 -0300

    revert changes

--- terraform/azure/tier0/guest_users/read_users.tf
+++ terraform/azure/tier0/guest_users/read_users.tf
@@ -8,13 +8,3 @@
 data "azuread_users" "return_all_users" {
   return_all = true
 }
-
-
-resource "azuread_user" "user" {
-  for_each = { for user in data.azuread_users.return_all_users.users : user.object_id => user }
-
-  user_principal_name = each.value.user_principal_name
-  display_name        = each.value.display_name
-  mail_nickname       = each.value.mail_nickname
-  account_enabled     = each.value.account_enabled
-}
