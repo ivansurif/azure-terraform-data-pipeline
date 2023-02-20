4e308cb9e97009f9895f729f3e82cafcb1ab500a
Ivan Surif <ivansurif@MacBook-Pro-4.local>
2023-02-19 21:43:58 -0300

    reattempt import with different approach

--- terraform/azure/tier0/guest_users/read_users.tf
+++ terraform/azure/tier0/guest_users/read_users.tf
@@ -1,5 +1,5 @@
 #resource "azuread_user" "example" {
-  # Add existing user to Terraform State File
+# Add existing user to Terraform State File
 #  user_principal_name = "cognite.test.user_gmail.com#EXT#@ivansurifgmail.onmicrosoft.com"
 #  display_name        = "Test User"
 #
@@ -7,4 +7,14 @@
 
 data "azuread_users" "return_all_users" {
   return_all = true
+}
+
+
+resource "azuread_user" "user" {
+  for_each = { for user in data.azuread_users.return_all_users.users : user.object_id => user }
+
+  user_principal_name = each.value.user_principal_name
+  display_name        = each.value.display_name
+  mail_nickname       = each.value.mail_nickname
+  account_enabled     = each.value.account_enabled
 }
