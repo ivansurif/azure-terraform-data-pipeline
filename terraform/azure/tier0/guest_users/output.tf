66477188215784bca730b38cec31d3264b386d2e
Ivan Surif <ivansurif@MacBook-Pro-4.local>
2023-02-19 21:30:11 -0300

    add output and remove target in apply

--- terraform/azure/tier0/guest_users/output.tf
+++ terraform/azure/tier0/guest_users/output.tf
@@ -2,4 +2,9 @@
 #  value = azuread_invitation.example
 #}
 
+output "all_users" {
+  value = {
+    for user in data.azuread_users.return_all_users.users : user.object_id => user.display_name
+  }
+}
 
