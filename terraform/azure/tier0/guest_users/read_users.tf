45bd31e838a71a152454ea9e6cb6dc2a7c19f4e3
Ivan Surif <ivansurif@MacBook-Pro-4.local>
2023-02-19 21:31:34 -0300

    comment out `azuread_user`

--- terraform/azure/tier0/guest_users/read_users.tf
+++ terraform/azure/tier0/guest_users/read_users.tf
@@ -1,16 +1,10 @@
-resource "azuread_user" "example" {
+#resource "azuread_user" "example" {
   # Add existing user to Terraform State File
-  user_principal_name = "cognite.test.user_gmail.com#EXT#@ivansurifgmail.onmicrosoft.com"
-  display_name        = "Test User"
-
-}
+#  user_principal_name = "cognite.test.user_gmail.com#EXT#@ivansurifgmail.onmicrosoft.com"
+#  display_name        = "Test User"
+#
+#}
 
 data "azuread_users" "return_all_users" {
   return_all = true
-}
-
-output "user_names" {
-  value = {
-    for user in data.azuread_users.return_all_users.users : user.user_principal_name => user.display_name
-  }
 }
