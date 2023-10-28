e52cfe8f91cbbc8d61d5e812b0bce6d8e39b32ab
Ivan  Surif <ivan.surif@cognitedata.com@Ivans-MacBook-Pro.local>
2023-10-28 11:57:58 -0300

    deploy terraform plan

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -20,10 +20,10 @@ provider "azurerm" {
   # Requires the following environment variables
   # ARM_CLIENT_SECRET
 
-  #client_id       = "d2e51d5d-9851-4045-bb80-f75cda8c4a14"
+  client_id       = "d2e51d5d-9851-4045-bb80-f75cda8c4a14"
   # client_secret   = "" Uses default variable name ARM_CLIENT_SECRET
-  #subscription_id = "2152bb9e-3b7e-4eef-a07d-36e8897d34aa"
-  #tenant_id       = "b8c39159-a389-4b78-993a-0c4d467c6229"
+  subscription_id = "2152bb9e-3b7e-4eef-a07d-36e8897d34aa"
+  tenant_id       = "b8c39159-a389-4b78-993a-0c4d467c6229"
   
   features {}
   skip_provider_registration = true
